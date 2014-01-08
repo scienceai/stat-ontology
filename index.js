f22f801d4153d90c957dd3b85a639a58c0c01f59
JDureau <dureau.joseph@gmail.com>
2014-01-08 19:17:48 +0100

    comments

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ exports.stats = {
       "@id": "stats:StatTest",
       "@type": "rdfs:Class",
       "label": "Statistical test",
+      "comment": "Statistical hypothesis test. A result is called statistically significant if it has been predicted as unlikely to have occurred by chance alone.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistical_hypothesis_testing",
       "subClassOf": [
         "schema:Intangible"
@@ -51,6 +52,7 @@ exports.stats = {
       "@id": "stats:TTest",
       "@type": "rdfs:Class",
       "label": "Statistical t-test",
+      "comment": "Statistical test in which the test statistic follows a Student t distribution under the null hypothesis.",
       "subClassOf": [
         "stats:StatTest"
       ],
@@ -60,6 +62,7 @@ exports.stats = {
       "@id": "stats:FTest",
       "@type": "rdfs:Class",
       "label": "Statistical F-test",
+      "comment": "Statistical test in which the test statistic follows an F-distribution under the null hypothesis.",
       "seeAlso": "http://en.wikipedia.org/wiki/T_test",
       "subClassOf": [
         "stats:StatTest"
@@ -70,6 +73,7 @@ exports.stats = {
       "@id": "stats:ZTest",
       "@type": "rdfs:Class",
       "label": "Statistical z-test",
+      "comment": "Statistical test in which the test statistic follows a normal distribution under the null hypothesis.",
       "seeAlso": "http://en.wikipedia.org/wiki/Z_test",
       "subClassOf": [
         "stats:StatTest"
@@ -80,6 +84,7 @@ exports.stats = {
       "@id": "stats:ChisqTest",
       "@type": "rdfs:Class",
       "label": "Statistical Chi-Square test",
+      "comment": "Chi-squared test, also referred to as chi-square test, is any statistical hypothesis test in which the sampling distribution of the test statistic is a chi-squared distribution under the null hypothesis.",
       "seeAlso": "http://en.wikipedia.org/wiki/Chi_square_test",
       "subClassOf": [
         "stats:StatTest"
@@ -91,6 +96,7 @@ exports.stats = {
       "@id": "stats:statistic",
       "@type": "rdf:Property",
       "label": "statistic",
+      "comment": "Numerical summary of a dataset used to perform a hypothesis test.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistic",
       "range": "xsd:float",
       "domain": "stats:StatTest",
@@ -100,6 +106,7 @@ exports.stats = {
       "@id": "stats:pValue",
       "@type": "rdf:Property",
       "label": "p-value",
+      "comment": "The p-value is the probability of obtaining a test statistic at least as extreme as the one that was actually observed, assuming that the null hypothesis is true.",
       "seeAlso": "http://en.wikipedia.org/wiki/P_value",
       "range": "xsd:float",
       "domain": "stats:StatTest",
@@ -109,6 +116,7 @@ exports.stats = {
       "@id": "stats:df",
       "@type": "rdf:Property",
       "label": "degrees of freedom",
+      "comment": "The number of degrees of freedom is the number of values in the final calculation of a statistic that are free to vary.",
       "seeAlso": "http://en.wikipedia.org/wiki/Degrees_of_freedom_(statistics)",
       "range": "xsd:integer",
       "domain": "stats:StatTest",
@@ -119,6 +127,7 @@ exports.stats = {
       "@id": "stats:dfNum",
       "@type": "rdf:Property",
       "label": "degrees of freedom",
+      "comment": "Number of degrees of freedom of the numerator.",
       "seeAlso": "http://en.wikipedia.org/wiki/Degrees_of_freedom_(statistics)",
       "range": "xsd:integer",
       "domain": "stats:FTest",
@@ -128,6 +137,7 @@ exports.stats = {
       "@id": "stats:dfDenom",
       "@type": "rdf:Property",
       "label": "degrees of freedom",
+      "comment": "Number of degrees of freedom of the denominator.",
       "seeAlso": "http://en.wikipedia.org/wiki/Degrees_of_freedom_(statistics)",
       "range": "xsd:integer",
       "domain": "stats:FTest",
@@ -138,6 +148,7 @@ exports.stats = {
       "@id": "stats:Statistic",
       "@type": "rdfs:Class",
       "label": "Statistic",
+      "comment": "Function of the sample.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistic",
       "subClassOf": [
         "stats:QuantitativeValue"
@@ -149,6 +160,7 @@ exports.stats = {
       "@id": "stats:estimate",
       "@type": "rdf:Property",
       "label": "Estimate",
+      "comment": "Numerical summary of a dataset.",
       "seeAlso": "http://en.wikipedia.org/wiki/Estimation_statistics",
       "range": "xsd:float",
       "domain": "stats:Statistic",
@@ -157,7 +169,8 @@ exports.stats = {
     {
       "@id": "stats:statTest",
       "@type": "rdf:Property",
-      "label": "Sgtatistical test",
+      "label": "Statistical test",
+      "comment": "Statistical hypothesis test. A result is called statistically significant if it has been predicted as unlikely to have occurred by chance alone.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistical_test",
       "range": "stats:StatTest",
       "domain": "stats:Statistic",
@@ -167,6 +180,7 @@ exports.stats = {
       "@id": "stats:stdError",
       "@type": "rdf:Property",
       "label": "Standard error",
+      "comment": "Standard deviation of the sampling distribution of a statistic.",
       "seeAlso": "http://en.wikipedia.org/wiki/Standard_error",
       "range": "xsd:float",
       "domain": "stats:Statistic",
@@ -177,6 +191,7 @@ exports.stats = {
       "@id": "stats:Correlation",
       "@type": "rdfs:Class",
       "label": "Correlation coefficient",
+      "comment": "Correlation refers to any of a broad class of statistical relationships involving dependence.",
       "seeAlso": "http://en.wikipedia.org/wiki/Correlation",
       "subClassOf": [
         "stats:Statistic"
@@ -188,6 +203,7 @@ exports.stats = {
       "@id": "stats:covariate1",
       "@type": "rdf:Property",
       "label": "First covariate",
+      "comment": "Variable that is observed.",
       "seeAlso": "http://en.wikipedia.org/wiki/Covariate",
       "range": "xsd:string",
       "domain": "stats:Correlation",
@@ -197,6 +213,7 @@ exports.stats = {
       "@id": "stats:covariate2",
       "@type": "rdf:Property",
       "label": "Second covariate",
+      "comment": "Variable that is observed.",
       "seeAlso": "http://en.wikipedia.org/wiki/Covariate",
       "range": "xsd:string",
       "domain": "stats:Correlation",
@@ -207,6 +224,7 @@ exports.stats = {
       "@id": "stats:Proportion",
       "@type": "rdfs:Class",
       "label": "Proportion",
+      "comment": "A part considered in relation to the whole.",
       "seeAlso": "http://en.wikipedia.org/wiki/Proportionality_(mathematics)",
       "subClassOf": [
         "stats:Statistic"
@@ -218,6 +236,7 @@ exports.stats = {
       "@id": "stats:StatModel",
       "@type": "rdfs:Class",
       "label": "Statistical model",
+      "comment": "Formalization of relationships between variables in the form of mathematical equations.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistical_model",
       "subClassOf": [
         "schema:Intangible"
@@ -229,6 +248,7 @@ exports.stats = {
       "@id": "stats:aic",
       "@type": "rdf:Property",
       "label": "Akaike Information Criterium",
+      "comment": "Measure of the relative quality of a statistical model, for a given set of data.",
       "seeAlso": "http://en.wikipedia.org/wiki/Akaike_information_criterion",
       "range": "xsd:float",
       "domain": "stats:StatModel",
@@ -239,6 +259,7 @@ exports.stats = {
       "@id": "stats:LinearModel",
       "@type": "rdfs:Class",
       "label": "Linear model",
+      "comment": "In a linear model, data are modeled using linear predictor functions, and unknown model parameters are estimated from the data.",
       "seeAlso": "http://en.wikipedia.org/wiki/Linear_model",
       "subClassOf": [
         "schema:StatModel"
@@ -250,6 +271,7 @@ exports.stats = {
       "@id": "stats:r2",
       "@type": "rdf:Property",
       "label": "R squared",
+      "comment": "Coefficient of determination, pronounced R squared, that indicates how well data points fit a line or curve.",
       "seeAlso": "http://en.wikipedia.org/wiki/R_squared",
       "range": "xsd:float",
       "domain": "stats:LinearModel",
@@ -259,6 +281,7 @@ exports.stats = {
       "@id": "stats:adjr2",
       "@type": "rdf:Property",
       "label": "adjusted R squared",
+      "comment": "The adjusted R2 is an attempt to take account of the phenomenon of the R2 automatically and spuriously increasing when extra explanatory variables are added to the model.",
       "seeAlso": "http://en.wikipedia.org/wiki/R_squared#Adjusted_R2",
       "range": "xsd:float",
       "domain": "stats:LinearModel",
@@ -268,6 +291,7 @@ exports.stats = {
       "@id": "stats:fRatioTest",
       "@type": "rdf:Property",
       "label": "F-Ratio Test",
+      "comment": " Used to identify the model that best fits the data.",
       "seeAlso": "http://en.wikipedia.org/wiki/F-test",
       "range": "stats:StatTest",
       "domain": "stats:LinearModel",
@@ -278,6 +302,7 @@ exports.stats = {
       "@id": "stats:modelFormula",
       "@type": "rdf:Property",
       "label": "Model formula",
+      "comment": "Concise way of expressing the hypothesised relation between explained and explanatory variables.",
       "seeAlso": "http://en.wikipedia.org/wiki/Formula",
       "range": "xsd:string",
       "domain": "stats:LinearModel",
@@ -287,6 +312,7 @@ exports.stats = {
       "@id": "stats:modelCoefficients",
       "@type": "rdf:Property",
       "label": "Model coefficients",
+      "comment": "Coefficients of the regression model.",
       "seeAlso": "http://en.wikipedia.org/wiki/Regression_coefficient",
       "range": "stats:Statistic",
       "domain": "stats:LinearModel",
@@ -296,6 +322,7 @@ exports.stats = {
       "@id": "stats:anova",
       "@type": "rdf:Property",
       "label": "anova",
+      "comment": "Collection of statistical models used to analyze the differences between group means and their associated procedures (such as 'variation' among and between groups).",
       "seeAlso": "http://en.wikipedia.org/wiki/Anova",
       "range": "stats:ANOVARow",
       "domain": "stats:LinearModel",
@@ -306,6 +333,7 @@ exports.stats = {
       "@id": "stats:ANOVARow",
       "@type": "rdfs:Class",
       "label": "ANOVA row",
+      "comment": "Row of an ANOVA",
       "seeAlso": "http://en.wikipedia.org/wiki/Anova",
       "subClassOf": [
         "stats:statistic"
@@ -317,6 +345,7 @@ exports.stats = {
       "@id": "stats:errorStratum",
       "@type": "rdf:Property",
       "label": "Error stratum",
+      "comment": "Error stratum of an ANOVA",
       "seeAlso": "http://en.wikipedia.org/wiki/Anova",
       "range": "xsd:string",
       "domain": "stats:ANOVARow",
@@ -326,6 +355,7 @@ exports.stats = {
       "@id": "stats:sumSq",
       "@type": "rdf:Property",
       "label": "Sum of Squares",
+      "comment": "Sum of squares.",
       "seeAlso": "http://en.wikipedia.org/wiki/Partition_of_sums_of_squares",
       "range": "xsd:float",
       "domain": "stats:ANOVARow",
@@ -335,6 +365,7 @@ exports.stats = {
       "@id": "stats:meanSq",
       "@type": "rdf:Property",
       "label": "Mean of Squares",
+      "comment": "Mean of squares.",
       "seeAlso": "http://en.wikipedia.org/wiki/Partition_of_sums_of_squares",
       "range": "xsd:float",
       "domain": "stats:ANOVARow",
@@ -345,6 +376,7 @@ exports.stats = {
       "@id": "stats:ANOVAResidual",
       "@type": "rdfs:Class",
       "label": "ANOVA factor",
+      "comment": "Difference between the observed value and the estimated function value.",
       "seeAlso": "http://en.wikipedia.org/wiki/Errors_and_residuals_in_statistics",
       "subClassOf": [
         "schema:ANOVARow"
@@ -356,6 +388,7 @@ exports.stats = {
       "@id": "stats:ANOVAFactor",
       "@type": "rdfs:Class",
       "label": "ANOVA factor",
+      "comment":"Process inputs an investigator manipulates to cause a change in the output.",
       "seeAlso": "http://en.wikipedia.org/wiki/Factor_analysis",
       "subClassOf": [
         "schema:ANOVARow"
@@ -367,6 +400,7 @@ exports.stats = {
       "@id": "stats:GeneralizedLinearModel",
       "@type": "rdfs:Class",
       "label": "Generalized linear model",
+      "comment":"Generalized linear model",
       "seeAlso": "http://en.wikipedia.org/wiki/Generalized_linear_model",
       "subClassOf": [
         "schema:LinearModel"
@@ -378,6 +412,7 @@ exports.stats = {
       "@id": "stats:modelFamily",
       "@type": "rdf:Property",
       "label": "Test parameters",
+      "comment":"Family of a generalized linear model.",
       "seeAlso": "http://en.wikipedia.org/wiki/Generalized_linear_model",
       "range": "xsd:string",
       "domain": "stats:GeneralisedLinearModel",
@@ -434,6 +469,7 @@ exports.datapackage = {
       "@id": "dpkg:repository",
       "@type": "rdf:Property",
       "label": "repository",
+      "comment":"Array of repositories where the package can be located.",
       "range": "schema:Code",
       "domain": "schema:DataCatalog",
       "status": "testing",
@@ -443,6 +479,7 @@ exports.datapackage = {
       "@id": "dpkg:path",
       "@type": "rdf:Property",
       "label": "path",
+      "comment":"Absolute or relative path.",
       "range": "xsd:string",
       "domain": "schema:Code",
       "status": "testing",
@@ -452,6 +489,7 @@ exports.datapackage = {
       "@id": "dpkg:analytics",
       "@type": "rdf:Property",
       "label": "analytics",
+      "comment":"List of analytics.",
       "range": "schema:Code",
       "domain": "schema:DataCatalog",
       "status": "testing",
@@ -461,6 +499,7 @@ exports.datapackage = {
       "@id": "dpkg:input",
       "@type": "rdf:Property",
       "label": "input",
+      "comment":"List of URLs of data used in a given analysis.",
       "range": "xsd:string",
       "domain": "schema:Code",
       "status": "testing",
@@ -470,6 +509,7 @@ exports.datapackage = {
       "@id": "dpkg:output",
       "@type": "rdf:Property",
       "label": "output",
+      "comment":"List of URLs of data generated in a given analysis.",
       "range": "xsd:string",
       "domain": "schema:Code",
       "status": "testing",
@@ -478,7 +518,8 @@ exports.datapackage = {
     {
       "@id": "dpkg:data",
       "@type": "rdf:Property",
-      "label": "Inline data of a data package resource",
+      "comment":"Inline data of a datapackage Dataset",
+      "label": "data",
       "range": "schema:DataDownload",
       "domain": "schema:DataSet",
       "status": "testing",
