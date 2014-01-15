9dd447165b94b20f6529d87be6d673e0d92336eb
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2014-01-15 12:38:32 -0500

    /

--- index.js
+++ index.js
@@ -429,7 +429,7 @@ exports.stats = {
 
 exports.datapackage = {
   "@context": {
-    "dpkg": "http://standardanalytics.io/datapackage#",
+    "dpkg": "http://standardanalytics.io/datapackage/",
     "schema": "http://schema.org/",
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
