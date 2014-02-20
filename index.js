009d2d614ed23f771e8fe1227456a79a10eb2b07
JDureau <dureau.joseph@gmail.com>
2014-02-20 12:44:10 +0100

    towards Bayesian Hierarchical Models

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ exports.stats = {
     },
 
     {
-      "@id": "stats:StatTest",
+      "@id": "stats:StatisticalTest",
       "@type": "rdfs:Class",
       "label": "Statistical test",
       "comment": "Statistical hypothesis test. A result is called statistically significant if it has been predicted as unlikely to have occurred by chance alone.",
@@ -66,7 +66,7 @@ exports.stats = {
       "label": "Statistical t-test",
       "comment": "Statistical test in which the test statistic follows a Student t distribution under the null hypothesis.",
       "subClassOf": [
-        "stats:StatTest"
+        "stats:StatisticalTest"
       ],
       "status": "testing"
     },
@@ -77,7 +77,7 @@ exports.stats = {
       "comment": "Statistical test in which the test statistic follows an F-distribution under the null hypothesis.",
       "seeAlso": "http://en.wikipedia.org/wiki/T_test",
       "subClassOf": [
-        "stats:StatTest"
+        "stats:StatisticalTest"
       ],
       "status": "testing"
     },
@@ -88,18 +88,18 @@ exports.stats = {
       "comment": "Statistical test in which the test statistic follows a normal distribution under the null hypothesis.",
       "seeAlso": "http://en.wikipedia.org/wiki/Z_test",
       "subClassOf": [
-        "stats:StatTest"
+        "stats:StatisticalTest"
       ],
       "status": "testing"
     },
     {
-      "@id": "stats:ChisqTest",
+      "@id": "stats:ChiSquareTest",
       "@type": "rdfs:Class",
       "label": "Statistical Chi-Square test",
       "comment": "Chi-squared test, also referred to as chi-square test, is any statistical hypothesis test in which the sampling distribution of the test statistic is a chi-squared distribution under the null hypothesis.",
       "seeAlso": "http://en.wikipedia.org/wiki/Chi_square_test",
       "subClassOf": [
-        "stats:StatTest"
+        "stats:StatisticalTest"
       ],
       "status": "testing"
     },
@@ -111,7 +111,7 @@ exports.stats = {
       "comment": "Numerical summary of a dataset used to perform a hypothesis test.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistic",
       "range": "xsd:float",
-      "domain": "stats:StatTest",
+      "domain": "stats:StatisticalTest",
       "status": "testing"
     },
     {
@@ -121,22 +121,22 @@ exports.stats = {
       "comment": "The p-value is the probability of obtaining a test statistic at least as extreme as the one that was actually observed, assuming that the null hypothesis is true.",
       "seeAlso": "http://en.wikipedia.org/wiki/P_value",
       "range": "xsd:float",
-      "domain": "stats:StatTest",
+      "domain": "stats:StatisticalTest",
       "status": "testing"
     },
     {
-      "@id": "stats:df",
+      "@id": "stats:degreesOfFreedom",
       "@type": "rdf:Property",
       "label": "Number of degrees of freedom",
       "comment": "The number of degrees of freedom is the number of values in the final calculation of a statistic that are free to vary.",
       "seeAlso": "http://en.wikipedia.org/wiki/Degrees_of_freedom_(statistics)",
       "range": "xsd:integer",
-      "domain": "stats:StatTest",
+      "domain": "stats:StatisticalTest",
       "status": "testing"
     },
 
     {
-      "@id": "stats:dfNum",
+      "@id": "stats:degreesOfFreedomfNumerator",
       "@type": "rdf:Property",
       "label": "Number of degrees of freedom",
       "comment": "Number of degrees of freedom of the numerator.",
@@ -146,7 +146,7 @@ exports.stats = {
       "status": "testing"
     },
     {
-      "@id": "stats:dfDenom",
+      "@id": "stats:degreesOfFreedomDenominator",
       "@type": "rdf:Property",
       "label": "Number of degrees of freedom",
       "comment": "Number of degrees of freedom of the denominator.",
@@ -169,17 +169,17 @@ exports.stats = {
     },
 
     {
-      "@id": "stats:statTest",
+      "@id": "stats:statisticalTest",
       "@type": "rdf:Property",
       "label": "Statistical test",
       "comment": "Statistical hypothesis test. A result is called statistically significant if it has been predicted as unlikely to have occurred by chance alone.",
       "seeAlso": "http://en.wikipedia.org/wiki/Statistical_test",
-      "range": "stats:StatTest",
+      "range": "stats:StatisticalTest",
       "domain": "stats:Statistic",
       "status": "testing"
     },
     {
-      "@id": "stats:stdError",
+      "@id": "stats:standardError",
       "@type": "rdf:Property",
       "label": "Standard error",
       "comment": "Standard deviation of the sampling distribution of a statistic.",
@@ -235,7 +235,7 @@ exports.stats = {
     },
 
     {
-      "@id": "stats:StatModel",
+      "@id": "stats:StatisticalModel",
       "@type": "rdfs:Class",
       "label": "Statistical model",
       "comment": "Formalization of relationships between variables in the form of mathematical equations.",
@@ -253,17 +253,17 @@ exports.stats = {
       "comment": "Concise way of expressing the hypothesised relation between explained and explanatory variables.",
       "seeAlso": "http://en.wikipedia.org/wiki/Formula",
       "range": "xsd:string",
-      "domain": "stats:LinearModel",
+      "domain": "stats:StatisticalModel",
       "status": "testing"
     },
     {
-      "@id": "stats:modelCalibration",
+      "@id": "stats:modelFit",
       "@type": "rdf:Property",
       "label": "Model fit",
-      "comment": "Calibration of the model parameters against the data.",
+      "comment": "Fit of model.",
       "seeAlso": "http://en.wikipedia.org/wiki/Calibration_(statistics)",
       "range": "stats:ModelCalibration",
-      "domain": "stats:StatModel",
+      "domain": "stats:StatisticalModel",
       "status": "testing"
     },
     {
@@ -273,15 +273,15 @@ exports.stats = {
       "comment": "Model parameter.",
       "seeAlso": "http://en.wikipedia.org/wiki/Model_parameter",
       "range": "stats:Parameter",
-      "domain": "stats:StatModel",
+      "domain": "stats:StatisticalModel",
       "status": "testing"
     },
 
     {
-      "@id": "stats:ModelCalibration",
+      "@id": "stats:ModelFit",
       "@type": "rdfs:Class",
-      "label": "Model calibration",
-      "comment": "Calibration of the model parameters against the data.",
+      "label": "Model Fit",
+      "comment": "Fit of the model.",
       "seeAlso": "http://en.wikipedia.org/wiki/Calibration_(statistics)",
       "subClassOf": [
         "schema:Intangible"
@@ -296,7 +296,7 @@ exports.stats = {
       "comment": "Coefficient of determination, pronounced R squared, that indicates how well data points fit a line or curve.",
       "seeAlso": "http://en.wikipedia.org/wiki/R_squared",
       "range": "xsd:float",
-      "domain": "stats:ModelCalibration",
+      "domain": "stats:ModelFit",
       "status": "testing"
     },
     {
@@ -306,60 +306,60 @@ exports.stats = {
       "comment": "The adjusted R2 is an attempt to take account of the phenomenon of the R2 automatically and spuriously increasing when extra explanatory variables are added to the model.",
       "seeAlso": "http://en.wikipedia.org/wiki/R_squared#Adjusted_R2",
       "range": "xsd:float",
-      "domain": "stats:ModelCalibration",
+      "domain": "stats:ModelFit",
       "status": "testing"
     },
 
     {
-      "@id": "stats:OptimalFit",
+      "@id": "stats:FitnessOptimization",
       "@type": "rdfs:Class",
       "label": "Optimal Fit",
       "comment": "Model fit obtained by maximizing a fitness criteria.",
       "seeAlso": "http://en.wikipedia.org/wiki/Maximum_likelihood",
       "subClassOf": [
-        "stats:ModelCalibration"
+        "stats:ModelFit"
       ],
       "status": "testing"
     },
 
     {
-      "@id": "stats:aic",
+      "@id": "stats:akaikeInformationCriterion",
       "@type": "rdf:Property",
       "label": "Akaike Information Criterion",
       "comment": "Measure of the relative quality of a statistical model, for a given set of data.",
       "seeAlso": "http://en.wikipedia.org/wiki/Akaike_information_criterion",
       "range": "xsd:float",
-      "domain": "stats:OptimalFit",
+      "domain": "stats:FitnessOptimization",
       "status": "testing"
     },
     {
-      "@id": "stats:bic",
+      "@id": "stats:bayesianInformationCriterion",
       "@type": "rdf:Property",
       "label": "Bayesian Information Criterion",
       "comment": "Measure of the relative quality of a statistical model, for a given set of data.",
       "seeAlso": "http://en.wikipedia.org/wiki/Bayesian_information_criterion",
       "range": "xsd:float",
-      "domain": "stats:OptimalFit",
+      "domain": "stats:FitnessOptimization",
       "status": "testing"
     },
     {
-      "@id": "stats:fitCriterion",
+      "@id": "stats:fitnessCriterion",
       "@type": "rdf:Property",
       "label": "fit Criterion",
       "comment": "Fitness criterion that has been optimized.",
       "seeAlso": "http://en.wikipedia.org/wiki/Likelihood_function",
       "range": "xsd:string",
-      "domain": "stats:OptimalFit",
+      "domain": "stats:FitnessOptimization",
       "status": "testing"
     },
     {
-      "@id": "stats:parameterEstimates",
+      "@id": "stats:optimalParameter",
       "@type": "rdf:Property",
-      "label": "optimalParameter",
-      "comment": "Optimal parameter.",
+      "label": "optimal Parameter",
+      "comment": "optimal Parameter.",
       "seeAlso": "http://en.wikipedia.org/wiki/Maximum_likelihood",
       "range": "stats:Parameter",
-      "domain": "stats:OptimalFit",
+      "domain": "stats:FitnessOptimization",
       "status": "testing"
     },
     {
@@ -369,7 +369,7 @@ exports.stats = {
       "comment": "Collection of statistical models used to analyze the differences between group means and their associated procedures (such as 'variation' among and between groups).",
       "seeAlso": "http://en.wikipedia.org/wiki/Anova",
       "range": "stats:ANOVARow",
-      "domain": "stats:OptimalFit",
+      "domain": "stats:FitnessOptimization",
       "status": "testing"
     },
 
@@ -380,13 +380,13 @@ exports.stats = {
       "comment": "Calibration of the model by Monte Carlo Markov Chain.",
       "seeAlso": "http://en.wikipedia.org/wiki/Markov_chain_Monte_Carlo",
       "subClassOf": [
-        "stats:ModelCalibration"
+        "stats:ModelFit"
       ],
       "status": "testing"
     },
 
     {
-      "@id": "stats:dic",
+      "@id": "stats:devianceInformationCriterion",
       "@type": "rdf:Property",
       "label": "Deviance Information Criterion",
       "comment": "Measure of the relative quality of a statistical model, for a given set of data.",
@@ -396,19 +396,29 @@ exports.stats = {
       "status": "testing"
     },
     {
-      "@id": "stats:trace",
+      "@id": "stats:mcmcTrace",
       "@type": "rdf:Property",
-      "label": "Trace",
-      "comment": "Trajectory of the Monte Carlo Markov Chaib.",
-      "seeAlso": "http://en.wikipedia.org/wiki/Markov_chain_Monte_Carlo",
+      "label": "MCMC Trace",
+      "comment": "MCMC trajectory in the parameter space.",
+      "seeAlso": "http://cran.r-project.org/web/packages/coda/coda.pdf",
       "range": "schema:Dataset",
       "domain": "stats:MCMC",
       "status": "testing"
     },
     {
-      "@id": "stats:ess",
+      "@id": "stats:mcmcAlgorithm",
       "@type": "rdf:Property",
-      "label": "Effective Sampling Size",
+      "label": "MCMC algorithm",
+      "comment": "MCMC algorithm.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Markov_chain_Monte_Carlo",
+      "range": "xsd:string",
+      "domain": "stats:MCMC",
+      "status": "testing"
+    },
+    {
+      "@id": "stats:effectiveSampleSize",
+      "@type": "rdf:Property",
+      "label": "Effective Sample Size",
       "comment": "Measure of the relative quality of a statistical model, for a given set of data.",
       "seeAlso": "http://cran.r-project.org/web/packages/coda/coda.pdf",
       "range": "xsd:float",
@@ -423,7 +433,7 @@ exports.stats = {
       "comment": "Model based on linear predictor functions.",
       "seeAlso": "http://en.wikipedia.org/wiki/Linear_model",
       "subClassOf": [
-        "stats:StatModel"
+        "stats:StatisticalModel"
       ],
       "status": "testing"
     },
@@ -451,7 +461,7 @@ exports.stats = {
       "status": "testing"
     },
     {
-      "@id": "stats:sumSq",
+      "@id": "stats:sumOfSquares",
       "@type": "rdf:Property",
       "label": "Sum of Squares",
       "comment": "Sum of squares.",
@@ -461,7 +471,7 @@ exports.stats = {
       "status": "testing"
     },
     {
-      "@id": "stats:meanSq",
+      "@id": "stats:meanOfSquares",
       "@type": "rdf:Property",
       "label": "Mean of Squares",
       "comment": "Mean of squares.",
@@ -525,8 +535,66 @@ exports.stats = {
       "comment":"Hierarchical Bayesian Model",
       "seeAlso": "http://en.wikipedia.org/wiki/Hierarchical_Bayesian_model",
       "subClassOf": [
-        "stats:StatModel"
+        "stats:StatisticalModel"
       ],
+      "status": "testing"
+    },
+
+    {
+      "@id": "stats:StateSpaceModel",
+      "@type": "rdfs:Class",
+      "label": "State Space Model",
+      "comment":"State Space Model",
+      "seeAlso": "http://www.scholarpedia.org/article/State_space_model",
+      "subClassOf": [
+        "stats:StatisticalModel"
+      ],
+      "status": "testing"
+    },
+
+    {
+      "@id": "stats:Prior",
+      "@type": "rdfs:Class",
+      "label": "Pior",
+      "comment":"Basyesian prior.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Prior_probability",
+      "subClassOf": [
+        "schema:CreativeWork"
+      ],
+      "status": "testing"
+    },
+
+    {
+      "@id": "stats:priorDistribution",
+      "@type": "rdf:Property",
+      "label": "Distribution parameter",
+      "comment":"Distribution parameter.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Prior_probability",
+      "range": "stats:Parameter",
+      "domain": "stats:Prior",
+      "status": "testing"
+    },
+
+    {
+      "@id": "stats:Posterior",
+      "@type": "rdfs:Class",
+      "label": "Probability Distribution",
+      "comment":"Probability distribution.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Posterior_probability",
+      "subClassOf": [
+        "schema:CreativeWork"
+      ],
+      "status": "testing"
+    },
+
+    {
+      "@id": "stats:posteriorDistribution",
+      "@type": "rdf:Property",
+      "label": "Posterior distribution",
+      "comment":"Posterior Distribution.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Posterior_probability",
+      "range": "stats:Parameter",
+      "domain": "stats:Posterior",
       "status": "testing"
     },
 
@@ -543,6 +611,26 @@ exports.stats = {
     },
     
     {
+      "@id": "stats:distributionType",
+      "@type": "rdf:Property",
+      "label": "Distribution type",
+      "comment":"Distribution type.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Probability_distribution",
+      "range": "xsd:string",
+      "domain": "stats:ProbabilityDistribution",
+      "status": "testing"
+    },
+    {
+      "@id": "stats:distributionParameter",
+      "@type": "rdf:Property",
+      "label": "Distribution parameter",
+      "comment":"Distribution parameter.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Parameter",
+      "range": "stats:Parameter",
+      "domain": "stats:ProbabilityDistribution",
+      "status": "testing"
+    },
+    {
       "@id": "stats:distributionParameter",
       "@type": "rdf:Property",
       "label": "Distribution parameter",
@@ -554,13 +642,24 @@ exports.stats = {
     },
 
     {
+      "@id": "stats:sampleSequence",
+      "@type": "rdf:Property",
+      "label": "Sample sequence",
+      "comment":"Sample sequence.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Sample_(statistics)",
+      "range": "schema:Dataset",
+      "domain": "stats:ProbabilityDistribution",
+      "status": "testing"
+    },
+
+    {
       "@id": "stats:Parameter",
       "@type": "rdfs:Class",
       "label": "Parameter",
       "comment":"Parameter",
       "seeAlso": "http://en.wikipedia.org/wiki/Parameter",
       "subClassOf": [
-        "schema:Intangible"
+        "schema:QuantitativeValue"
       ],
       "status": "testing"
     },
@@ -575,6 +674,18 @@ exports.stats = {
       "domain": "stats:Parameter",
       "status": "testing"
     },
+
+    {
+      "@id": "stats:posterior",
+      "@type": "rdf:Property",
+      "label": "Posterior distribution",
+      "comment":"Posterior distribution.",
+      "seeAlso": "http://en.wikipedia.org/wiki/Posterior_probability",
+      "range": "stats:ProbabilityDistribution",
+      "domain": "stats:Parameter",
+      "status": "testing"
+    },
+
     {
       "@id": "stats:estimate",
       "@type": "rdf:Property",
